0eba00a28bc89d934a8e8af5cbf252d044649689
KrisRozenberg <91516205+KrisRozenberg@users.noreply.github.com>
2022-04-04 22:41:06 +0300

    Delete src/main/java/com/task/springshop/model/entity directory

--- src/main/java/com/task/springshop/model/entity/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.task.springshop.model.entity;
-
-public enum OrderStatus {
-    IN_PROCESS,
-    DONE,
-    REJECTED
-}
--- src/main/java/com/task/springshop/model/entity/ProductStatus.java
+++
@@ -1,6 +0,0 @@
-package com.task.springshop.model.entity;
-
-public enum ProductStatus {
-    IN_STOCK,
-    OUT_OF_STOCK
-}
